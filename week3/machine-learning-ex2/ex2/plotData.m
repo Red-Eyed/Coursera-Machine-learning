f47e81ca3fcd14d51cc3d6255c0ddcfd50ed1971
Vadim-Stupakov <vadim.stupakov@gmail.com>
2017-04-09 17:49:43 +0300

    Implemented plotData func

--- week3/machine-learning-ex2/ex2/plotData.m
+++ week3/machine-learning-ex2/ex2/plotData.m
@@ -12,6 +12,12 @@ figure; hold on;
 %               examples and 'ko' for the negative examples.
 %
 
+% Find Indices of Positive and Negative Examples
+pos = find(y==1); neg = find(y == 0);
+
+% Plot Examples
+plot(X(pos, 1), X(pos, 2), 'g+', 'LineWidth', 2, 'MarkerSize', 10);
+plot(X(neg, 1), X(neg, 2), 'ro', 'LineWidth', 2, 'MarkerSize', 10);
 
 % =========================================================================
 
